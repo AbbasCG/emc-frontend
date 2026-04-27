62b8d4c4912c40ea1e63e006f230c1dfb4006b75
Abbas <abas_235@hotmail.com>
2026-04-27 20:27:10 +0200

    initial frontend setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "#2691C2",
+        customOrange: "#ec943c",
+        deepBlue: "#22334a",
+      },
+    },
+  },
+  plugins: [],
+}
